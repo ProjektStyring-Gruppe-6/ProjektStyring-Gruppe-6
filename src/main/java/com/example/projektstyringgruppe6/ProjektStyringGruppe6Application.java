0f1845cc5152bfc1cb71c5f7348f73051d739f0b
lindkvst <lindkviste@gmail.com>
2026-04-29 11:32:42 +0200

    Initial commit

---
+++ src/main/java/com/example/projektstyringgruppe6/ProjektStyringGruppe6Application.java
@@ -0,0 +1,13 @@
+package com.example.projektstyringgruppe6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektStyringGruppe6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektStyringGruppe6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektstyringgruppe6/ProjektStyringGruppe6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektstyringgruppe6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektStyringGruppe6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
